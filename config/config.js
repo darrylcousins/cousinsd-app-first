8b29d4be11dfd62fcda88c4e39414a814a9e55fe
Darryl Cousins <darryljcousins@gmail.com>
2020-06-12 10:22:38 +1200

    Added db credentials for production

--- config/config.js
+++ config/config.js
@@ -18,11 +18,11 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
     "operatorsAliases": false
   }
 }
