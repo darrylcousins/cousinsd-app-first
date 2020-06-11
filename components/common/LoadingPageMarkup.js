6d8997bdb10ee613481a03d64b9145e7a77d2e9e
Darryl Cousins <darryljcousins@gmail.com>
2020-06-12 01:07:43 +1200

    fix for heroku build

--- components/common/LoadingPageMarkup.js
+++ components/common/LoadingPageMarkup.js
@@ -8,7 +8,7 @@ import {
   Spinner,
 } from '@shopify/polaris';
 
-export function LoadingPageMarkup() {
+export default function LoadingPageMarkup() {
   return (
     <Layout>
       <Layout.Section>
