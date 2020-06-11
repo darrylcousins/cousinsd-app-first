6d47baa523bfc80d648235f7f1117bf93f591160
Darryl Cousins <darryljcousins@gmail.com>
2020-06-12 11:14:13 +1200

    Heroku db config

--- config/config.js
+++ config/config.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "host": process.env.DB_HOSTNAME,
+    "dialect": process.env.DB_DIALECT,
+    "operatorsAliases": false
+  },
+  "local": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOSTNAME,
     "dialect": process.env.DB_DIALECT,
     "operatorsAliases": false
   },
@@ -18,11 +26,11 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": process.env.DB_USER,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "operatorsAliases": false
+    "host": process.env.DB_HOSTNAME,
+    "dialect": "postgres",
+    "use_env_variable": 'DATABASE_URL'
   }
 }
--- config/env.config.js
+++ config/env.config.js
@@ -9,10 +9,10 @@ const ENV = {
 
   SHOP_ID: process.env.SHOP_ID,
 
-  DB_HOST: process.env.DB_HOST,
+  DB_HOSTNAME: process.env.DB_HOSTNAME,
   DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
+  DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_DIALECT: process.env.DB_DIALECT,
 
